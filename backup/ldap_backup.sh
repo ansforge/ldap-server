41751d7722bb42325dbdec16ce1d979e4b42e3bb
Michaël Faurel <12185901+mfaurel@users.noreply.github.com>
2024-08-28 16:54:54 +0200

    Update ldap_backup.sh

--- backup/ldap_backup.sh
+++ backup/ldap_backup.sh
@@ -4,7 +4,7 @@ echo "Démarrage du script de sauvegarde du LDAP de la Forge ANS"
 # Nom du script     : ldap-backup.sh
 # Auteur            : E.RIEGEL (QM HENIX)
 # Date de Création  : 15/12/2022
-# Version           : 0.0.2
+# Version           : 1.0.0
 # Descritpion       : Script permettant la sauvegarde du LDAP de la Forge ANS
 #
 # Historique des mises à jour :
@@ -15,6 +15,8 @@ echo "Démarrage du script de sauvegarde du LDAP de la Forge ANS"
 #-----------+--------+-------------+------------------------------------------------------
 #  0.0.2    | 21/09/23 | Y.ETRILLARD      | Modification du nom du JOB
 #-----------+--------+-------------+------------------------------------------------------
+#  1.0.0    | 28/08/24 | M. FAUREL   | Modification de la casse du path et timestamp
+#-----------+--------+-------------+------------------------------------------------------
 #
 ###############################################################################################
 
@@ -22,9 +24,10 @@ echo "Démarrage du script de sauvegarde du LDAP de la Forge ANS"
 
 # Configuration de base: datestamp e.g. YYYYMMDD
 DATE=$(date +"%Y%m%d")
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Dossier où sauvegarder les backups
-BACKUP_DIR="/var/backup/LDAP"
+BACKUP_DIR="/var/backup/ldap"
 
 # Commande NOMAD
 #NOMAD=/usr/local/bin/nomad
@@ -33,11 +36,11 @@ NOMAD=$(which nomad)
 #LDAP PATH To BACKUP in the container
 LDAP_PATH=/bitnami
 #Archive Name of the backup LDAP directory
-BACKUP_REPO_FILENAME="BACKUP_LDAP_${DATE}.tar.gz"
+BACKUP_REPO_FILENAME="backup_ldap_${DATE}.tar.gz"
 
 
 # Nombre de jours à garder les dossiers (seront effacés après X jours)
-RETENTION=5
+RETENTION=10
 
 # ---- NE RIEN MODIFIER SOUS CETTE LIGNE ------------------------------------------
 #
@@ -45,19 +48,19 @@ RETENTION=5
 mkdir -p $BACKUP_DIR/$DATE
 
 # Backup LDAP
-echo "Starting backup ldap..."
+echo "${TIMESTAMP} Starting backup ldap..."
 
 $NOMAD exec -task openldap -job  openldap-forge tar -cOzv -C $LDAP_PATH openldap > $BACKUP_DIR/$DATE/$BACKUP_REPO_FILENAME
 BACKUP_RESULT=$?
 if [ $BACKUP_RESULT -gt 1 ]
 then
-        echo "LDAP backup failed with error code : ${BACKUP_RESULT}"
+        echo "${TIMESTAMP} Backup LDAP failed with error code : ${BACKUP_RESULT}"
         exit 1
 else
-        echo "LDAP backup done"
+        echo "${TIMESTAMP} Backup LDAP done"
 fi
 
 # Remove files older than X days
 find $BACKUP_DIR/* -mtime +$RETENTION -exec rm -rf {} \;
 
-echo "Backup LDAP finished"
+echo "${TIMESTAMP} Backup LDAP finished"
